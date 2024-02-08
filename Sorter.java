9ab728cb2dba94795f3eea9e8b83b976cdf716c5
Rawan20000 <rawan_rz@icloud.com>
2024-02-08 15:22:42 +0300

    woww-a

--- Sorter.java
+++ Sorter.java
@@ -1,22 +1,22 @@
-public class Sorter {
-    public static void main(String[] args) {
-    
-       publics static void nsertionSiort(int arr[]) 
-        int n = arr.length; 
-        for ( i = 1; i < n; ++i) { 
-            int key = arr[i]; 
-            int j = i - 1; 
-  
-            // Move elements of arr[0..i-1], that are 
-            // greater than key, to one position ahead 
-            // of their current position 
-            while (j >= 0 && arr[j] > key) { 
-                arr[j + 1] = arr[j]; 
-                j = j - 1; 
-            } 
-            arr[j + 1] = key; 
-        } 
-       
-    } 
-        
+public class Haha {
+    public static void main (String[] args) {
+    }
+    public static void insertion_sort(int arr[]) {
+        int n = arr.length;
+
+        // loop for each iteration
+        for (int i = 1; i < n; i++) {
+
+            // storing value of arr[i] in key temporarily
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move all the elements to the right side of the key which are greater than key value.
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
