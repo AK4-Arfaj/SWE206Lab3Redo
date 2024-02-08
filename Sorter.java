494d12a632075ca5e9e67b3820ff08290b50c61e
[AK4_Arfaj] <[ak4_arfaj@gmail.com]>
2024-02-08 15:27:14 +0300

    ok-a

--- Sorter.java
+++ Sorter.java
@@ -1,22 +1,20 @@
-public class Haha {
-    public static void main (String[] args) {
+public class Sorter {
+    public static void main(String[] args) {
     }
-    public static void insertion_sort(int arr[]) {
-        int n = arr.length;
 
-        // loop for each iteration
-        for (int i = 1; i < n; i++) {
-
-            // storing value of arr[i] in key temporarily
-            int key = arr[i];
-            int j = i - 1;
-
-            // Move all the elements to the right side of the key which are greater than key value.
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
-    }
-}
+    public static void selectionSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }  
+} 
+        
